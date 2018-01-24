3cb9105172c409084999c05f1786b2b6421bb850
U-YXY\yeoxi <yeoxingyee30@gmail.com>
2018-01-24 20:25:08 +0800

    minor additions and stuff

--- iembp.js
+++ iembp.js
@@ -30,6 +30,25 @@ else if(x) {
     setTimeout(autoread(), 500);
   }
 }
+
+
+
+var firstTime = localStorage.getItem("iEMBFirst");
+    if(window.location.href.indexOf("iemb.hci.edu.sg") > -1){
+        if(!firstTime){
+        window.alert("Welcome to iEMB+!\nThis is a mini tutorial on how to use this script!\n(Click OK to continue)");
+        window.alert("Firstly, the read all button will read everything! (Refresh after the reload to make sure everything's read) \nTip: 1/3");
+        window.alert("Next, you can toggle dark/light mode by clicking the button in the top menu! \nTip: 2/3");
+        window.alert("Lastly, change your profile picture by clicking on it! \nTip: 3/3");
+        window.alert("And... that's it! If you have any questions / feedback contact me at \"yeoxingyee30@gmail.com\" with the subject heading of \"iEMB+ Feedback!\"\nEnjoy!");
+        localStorage.setItem("iEMBFirst", "1");
+    }
+    var dp = localStorage.getItem("profilePic");
+    if(!dp){
+        dp = window.prompt("Profile pic not set! \n Please enter a direct link to the image that you want for your profile picture!");
+        $('.iemb_user_left>img').attr('src', dp);
+        localStorage.setItem("profilePic", dp);
+    }
     function setDP(){
     var dp = localStorage.getItem('profilePic');
     $('.iemb_user_left>img').attr('src', dp);
